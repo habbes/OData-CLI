e9393eeeda2757434573fb3ea06dc400b4d38cb8
Clement Habinshuti <clhabins@microsoft.com>
2020-07-29 18:49:21 +0300

    Add Csdl updater

---
+++ ODataCLI/CsdlUpdater.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace ODataCLI
+{
+    class CsdlUpdater
+    {
+        public static void UpdateProjectConfig(string projectRoot, string csdlFile, string connectionString)
+        {
+            string mainProject = "Hackathon2020.Poc01";
+
+            var csdlTarget = Path.Combine(projectRoot, mainProject, "project.csdl");
+            var connStringTarget = Path.Combine(projectRoot, mainProject, "connectionstring.txt");
+
+            File.Copy(csdlFile, csdlTarget, true);
+            File.WriteAllText(connStringTarget, connectionString);
+        }
+    }
+}
